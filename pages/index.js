a039432d98a935e062532c7598781491e17f66c4
cruzetapiko <simonetvia@outlook.com>
2023-03-19 20:17:27 -0600

    test

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,11 @@
+import Link from 'next/link';
+
 export default function Home() {
-  return <h1 className="text-center underline">hola hombre</h1>;
+  return (
+    <div className="bg-slate-500">
+      <h1 className="text-center underline">
+        Welcome to <Link href="/posts/first-post">first_link</Link>
+      </h1>
+    </div>
+  );
 }
---
+++ pages/posts/first-post.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function FirstPost() {
+  return (
+    <div>
+      <h1>First Post</h1>
+      <Link href="/">back_home</Link>
+    </div>
+  );
+}
