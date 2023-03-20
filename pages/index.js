85535cf7c3d582749450450b2ed2f16a992c6c5f
cruzetapiko <simonetvia@outlook.com>
2023-03-20 16:47:29 -0600

    job-tailwind

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,8 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="bg-slate-500">
-      <h1 className="text-center underline">
+    <div className="bg-slate-500 ">
+      <h1 className="text-center underline text-sky-400/100">
         Welcome to <Link href="/posts/first-post">first_link</Link>
       </h1>
     </div>
