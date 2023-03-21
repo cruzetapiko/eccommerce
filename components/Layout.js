9bd8ae6012f9f34717a7e03eed2017013bb81f9d
cruzetapiko <simonetvia@outlook.com>
2023-03-20 20:41:55 -0600

    productitem

--- components/Layout.js
+++ components/Layout.js
@@ -1,29 +1,36 @@
-import React from "react";
-import Head from "next/head";
-import Link from "next/link";
+import Head from 'next/head';
+import Link from 'next/link';
 
 export default function Layout({ title, children }) {
   return (
     <>
       <Head>
-        <title>{title ? title + "-hola" : "Amazona"}</title>
+        <title>{title ? title + '-hola' : 'Amazona'}</title>
         <meta name="description" content="Home page" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="flex min-h-screen flex-col justify-between">
         <header>
-          <nav className=" flex h-12 items-center justify-between shadow-md">
+          <nav className=" flex h-12 items-center px-4 justify-between shadow-md">
             <Link href="/" className="text-lg font-bold">
               Amazona
             </Link>
             <div>
-              <Link href="/cart">Cart</Link>
-              <Link href="/signin">Sign In</Link>
+              <Link className="p-2" href="/cart">
+                Cart
+              </Link>
+              <Link className="p-2" href="/signin">
+                Login
+              </Link>
             </div>
           </nav>
         </header>
-        <main>{children}</main>
-        <footer>footer</footer>
+        <body className="bg-slate-300">
+          <main className="container m-auto mt-4 px-4 ">{children}</main>
+        </body>
+        <footer className="flex h-10 justify-center items-center shadow-inner ">
+          footer
+        </footer>
       </div>
     </>
   );
---
+++ components/ProductItem.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+
+export default function ProductItem({ product }) {
+  return (
+    <div className="card">
+      <Link href={`/product/${product.slug}`}>
+        <Image
+          src={product.image}
+          alt={product.name}
+          className="rounded shadow object-cover h-auto w-full"
+          width={100}
+          height={100}
+          priority
+        />
+      </Link>
+      <div className="flex flex-col items-center justify-center p-5">
+        <Link href={`/product/${product.slug}`}>
+          <h2 className="text-lg">{product.name}</h2>
+        </Link>
+        <p className="mb-2">{product.brand}</p>
+        <p>${product.price}</p>
+        <button
+          className="primary-button"
+          type="button"
+          onClick={() => addToCartHandler(product)}
+        >
+          Add to cart
+        </button>
+      </div>
+    </div>
+  );
+}
